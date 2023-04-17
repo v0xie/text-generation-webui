4a4f42de1d78c745b3b64bbadf7dff0b64c685cb
v0xie <28695009+v0xie@users.noreply.github.com>
2023-04-17 01:45:57 -0700

    Fix early truncation if substring already exists in reply

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -165,11 +165,12 @@ def generate_reply(question, state, eos_token=None, stopping_strings=[]):
                 # No need to generate 8 tokens at a time.
                 for reply in shared.model.generate_with_streaming(context=question, **generate_params):
                     output = original_question + reply
+                    original_reply = reply
                     if not shared.is_chat():
                         reply = original_question + apply_extensions(reply, 'output')
-                    found_stop_string = next((s for s in evaluated_stopping_strings if s in reply), None)
+                    found_stop_string = next((s for s in evaluated_stopping_strings if s in original_reply), None)
                     if found_stop_string:
-                        yield formatted_outputs(reply.partition(found_stop_string)[0], shared.model_name)
+                        yield formatted_outputs(str(reply).rsplit(found_stop_string, 1)[0], shared.model_name)
                         break
                     yield formatted_outputs(reply, shared.model_name)
 
