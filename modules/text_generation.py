1b6b4b932042040b3b69153f6705d2553bfd3709
v0xie <28695009+v0xie@users.noreply.github.com>
2023-04-17 02:05:42 -0700

    Check if stop string in stopping_strings OR evaluated_stopping_strings

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -169,7 +169,7 @@ def generate_reply(question, state, eos_token=None, stopping_strings=[]):
                     if not shared.is_chat():
                         original_reply = apply_extensions(reply, 'output')
                         reply = original_question + original_reply
-                    found_stop_string = next((s for s in evaluated_stopping_strings if s in original_reply), None)
+                    found_stop_string = next((s for s in stopping_strings + evaluated_stopping_strings if s in original_reply), None)
                     if found_stop_string:
                         yield formatted_outputs(str(reply).rsplit(found_stop_string, 1)[0], shared.model_name)
                         break
